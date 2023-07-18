8892d5cdec4d2aeb2c9adde035646169487c8798
EYadroshnikov <yadegor1375@gmail.com>
2023-07-18 19:19:12 +0600

    refactored

---
+++ getData.py
@@ -0,0 +1,12 @@
+import requests
+
+
+def get_weather(city):
+    try:
+        r = requests.get(
+            f"https://api.openweathermap.org/data/2.5/weather?q={city}&lang=ru&appid={cfg.api_key}&units=metric")
+        json = r.json()
+        return json
+    except Exception as ex:
+        print(ex)
+        print("invalid city:", city)
---
+++ message_constr.py
@@ -0,0 +1,48 @@
+import requests
+import datetime
+
+import getData
+
+
+def invalid_city(message):
+    return f"Город {message.text} не найден, попробуйте другой"
+    # bot.send_message(message.chat.id, f"Город {message.text} не найден, попробуйте другой")
+
+
+def weather_emodzy(city):
+    r = requests.get(
+        f"https://api.openweathermap.org/data/2.5/weather?q={city}&appid={cfg.api_key}&units=metric")
+    json = r.json()
+    des = json["weather"][0]["description"]
+    # print(des)
+    if des == "clear sky":
+        return "☀"
+    elif des == "few clouds" or "broken clouds":
+        return "⛅"
+    elif des == "scattered clouds":
+        return "☁"
+    elif des == "overcast clouds":
+        return "☁"
+    elif des == "shower rain":
+        return "🌧"
+    elif des == "light rain" or "moderate rain":
+        return "🌦"
+    elif des == "thunderstorm":
+        return "⛈"
+    elif des == "snow":
+        return "❄"
+    elif des == "mist":
+        return "🌫"
+
+
+def weather(message):
+    data = getData.get_weather(message.text)
+    if data['cod'] != 200:
+        return invalid_city(message), data['cod']
+    else:
+        return f"* ---------- {datetime.datetime.now().strftime('%Y-%m-%d %H:%M')} ---------- *\n" \
+               f"{weather_emodzy(data['name'])} Погода в г. {data['name']}: {data['weather'][0]['description']} {weather_emodzy(data['name'])}\n" \
+               f"🌡 Температура: {data['main']['temp']}°C ({data['main']['feels_like']}°C) 🌡\n " \
+               f"💧 Влажность: {data['main']['humidity']}% 💧\n" \
+               f"🌪 Скорость ветра: {data['wind']['speed']} м/с 🌪\n" \
+               f"🕰 Давление: {data['main']['pressure']} мм рт. ст. 🕰", data['cod']
